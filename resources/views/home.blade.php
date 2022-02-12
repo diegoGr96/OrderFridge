390671fa1055260e55fc785d844f2283f048a058
Diego Garcia <diego_9696c@hotmail.com>
2022-02-12 20:57:55 +0100

    Updated basic settings and chnages to the app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h1>{{ __('Index') }}</h1>
                 </div>
             </div>
         </div>
